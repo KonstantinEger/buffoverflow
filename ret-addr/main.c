8feccd916ee075c1c6b0d0e39a20bc61082820f6
KonstantinEger <35273325+KonstantinEger@users.noreply.github.com>
2024-01-21 15:45:55 +0100

    remove old stuff from ret-addr

--- ret-addr/main.c
+++ ret-addr/main.c
@@ -2,28 +2,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void print_buff(uint8_t * buffer, size_t n) {
-    for (size_t i = 0; i < n; i++) {
-        printf("%02X ", buffer[i]);
-        if ((i+1) % 8 == 0) {
-            printf("\n");
-        }
-    }
-    printf("\n");
-}
-
-void flag() {
+void flag(void) {
     printf("Congratulations, you reached the flag!!!\n");
     exit(0);
 }
 
-uint8_t add(uint8_t a, uint8_t b) {
+void do_something_with_input(void) {
     char data[] = { 0xde, 0xad, 0xbe, 0xef };
+    printf("Please give your input:\n");
     scanf("%[^\n]", data);
-    return a + b;
 }
 
 int main() {
-    add(5, 4);
-    printf("H\n");
+    do_something_with_input();
 }
